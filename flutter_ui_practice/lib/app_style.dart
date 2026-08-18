94d4538a45c9ecf8f2a8b081d36a6fa3b9400968
Ashikur Rahman Nihal <arnihal.cs@gmail.com>
2026-08-18 23:24:12 +0600

    Update app_style.dart
    Reduced font size of headlineStyle5 from 22 to 20.

--- flutter_ui_practice/lib/app_style.dart
+++ flutter_ui_practice/lib/app_style.dart
@@ -32,7 +32,7 @@ class Styles {
     fontWeight: FontWeight.w500,
   );
   static TextStyle headlineStyle5 = TextStyle(
-    fontSize: 22,
+    fontSize: 20,
     color: blackColor,
     fontWeight: FontWeight.w500,
   );
