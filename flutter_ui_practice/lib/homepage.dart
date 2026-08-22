082e7b18c2e35587969e395d2bd689bd336a7368
Ashikur Rahman Nihal <arnihal.cs@gmail.com>
2026-08-22 23:32:55 +0600

    Update homepage.dart

--- flutter_ui_practice/lib/homepage.dart
+++ flutter_ui_practice/lib/homepage.dart
@@ -9,7 +9,7 @@ class Homepage extends StatelessWidget {
       backgroundColor: Colors.blue,
       body: Column(
         children: [
-          Text("Hello Welcome"),
+          Text("Hello"),
           Text("Login to continue"),
           TextField(decoration: InputDecoration(hintText: "Username")),
           TextField(decoration: InputDecoration(hintText: "Password")),
