fe43941f8c31be6148ba3e44ec4366106a9de751
Ashikur Rahman Nihal <arnihal.cs@gmail.com>
2026-08-30 21:18:03 +0600

    Update app_style.dart

--- flutter_ui_practice/lib/app_style.dart
+++ flutter_ui_practice/lib/app_style.dart
@@ -16,7 +16,7 @@ Color greyColor = Colors.grey;
 
 class Styles {
   static TextStyle headlineStyle1 = TextStyle(
-    fontSize: 15,
+    fontSize: 16,
     color: blackColor,
     fontWeight: FontWeight.w500,
   );
